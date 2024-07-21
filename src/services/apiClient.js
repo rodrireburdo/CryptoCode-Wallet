dff18bf23cfac150a4983d4a4a7337c0a50b2985
Rodri Reburdo <rodri.reburdo01@gmail.com>
2024-07-21 20:11:43 -0300

    Agregado comprobacion de usuario y modales en componenete editar

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://laboratorio3-5459.restdb.io/rest/',
+    baseURL: 'https://labor3-d60e.restdb.io/rest/',
     headers: {
         'x-apikey': '64a2e9bc86d8c525a3ed8f63'
     }
